116c9cdf81a1df326aa8c9c36c2ca0d56f9373c2
Arjun-Shukla <arjunhp14s@gmail.com>
2025-12-19 22:30:13 +0530

    added car movement

--- sheet/js/index.js
+++ sheet/js/index.js
@@ -1,17 +1,29 @@
-let car = document.getElementById('car');
-car.focus();
+let car = document.getElementById("car");
+// car.focus();
+let margin = 70;
+let pos = 520;
 
-car.addEventListener('keydown' , function left(e){
-if(e.key=="ArrowLeft"){
-    car.style.position='relative';
-    car.style.top='0rem';
-    car.style.left='45rem';
+document.addEventListener("keydown", function move(e) {
+  console.log(e.keyCode);
 
-}
-})
+  if (e.keyCode == 37) {
+    pos -= margin;
+  } else if (e.keyCode == 39) {
+    pos += margin;
+  }
+  // else if(pos<0 || pos>1040){
+  //     break;
+  // }
+  if (pos < 260) {
+    pos = 260;
+  } 
+  
+  if (pos > 788) {
+    pos = 788;
+  }
+  // car.style.position = 'absolute';
+  car.style.left = pos + "px";
+  car.style.bottom = "0px";
 
-
-
-
-
-
+  // setInterval(move,100);
+});
