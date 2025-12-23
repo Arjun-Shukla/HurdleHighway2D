16ea8978828f45e7ae16bcc456aa600809e47987
Arjun-Shukla <arjunhp14s@gmail.com>
2025-12-24 01:19:54 +0530

    added all obs Y axis crash logic

--- sheet/js/index.js
+++ sheet/js/index.js
@@ -40,25 +40,25 @@ let obs1 = document.getElementById("obs1");
 
 //animation for obs1
 
-let f1 = function obsanimate(element) {
-  element.classList.remove("animateobs1");
+let f1 = function obsanimate1() {
+  obs1.classList.remove("animateobs1");
 
   setTimeout(() => {
-    element.classList.add("animateobs1");
+    obs1.classList.add("animateobs1");
   }, 10);
 };
 
-f1(obs1);
+f1();
 
 let clear1 = setInterval(() => {
-  f1(obs1);
+  f1();
 }, 2500);
 
 //animation for obs2
 
 let obs2 = document.getElementById("obs2");
 
-function obsanimate2() {
+let f2 =function obsanimate2() {
   obs2.classList.remove("animateobs2");
 
   setTimeout(() => {
@@ -66,15 +66,15 @@ function obsanimate2() {
   }, 3000);
 }
 
-// obsanimate2();
+// f2();
 
-// setInterval(obsanimate2, 9000);
+// let clear2 = setInterval(f2, 9000);
 
 //animation for obs3
 
 let obs3 = document.getElementById("obs3");
 
-function obsanimate3() {
+let f3 =function obsanimate3() {
   obs3.classList.remove("animateobs3");
 
   setTimeout(() => {
@@ -82,15 +82,15 @@ function obsanimate3() {
   }, 2000);
 }
 
-// obsanimate3();
+// f3();
 
-// setInterval(obsanimate3, 8000);
+// let clear3 = setInterval(f3, 8000);
 
 //animation for obs4
 
 let obs4 = document.getElementById("obs4");
 
-function obsanimate4() {
+let f4 = function obsanimate4() {
   obs4.classList.remove("animateobs4");
 
   setTimeout(() => {
@@ -98,15 +98,15 @@ function obsanimate4() {
   }, 2000);
 }
 
-// obsanimate4();
+// f4();
 
-// setInterval(obsanimate4, 7000);
+//let clear4 = setInterval(f4, 7000);
 
 //animation for obs5
 
 let obs5 = document.getElementById("obs5");
 
-function obsanimate5() {
+let f5 = function obsanimate5() {
   obs5.classList.remove("animateobs5");
 
   setTimeout(() => {
@@ -114,15 +114,15 @@ function obsanimate5() {
   }, 2500);
 }
 
-// obsanimate5();
+// f5();
 
-// setInterval(obsanimate5, 8500);
+// let clear5 = setInterval(f5, 8500);
 
 //animation for obs7
 
 let obs7 = document.getElementById("obs7");
 
-function obsanimate7() {
+let f7 = function obsanimate7() {
   obs7.classList.remove("animateobs7");
 
   setTimeout(() => {
@@ -130,15 +130,15 @@ function obsanimate7() {
   }, 10000);
 }
 
-// obsanimate7();
+// f7();
 
-// setInterval(obsanimate7, 13000);
+// let clear7 = setInterval(f7, 13000);
 
 // animation for obs 9
 
 let obs9 = document.getElementById("obs9");
 
-function obsanimate9() {
+let f9 = function obsanimate9() {
   obs9.classList.remove("animateobs9");
 
   setTimeout(() => {
@@ -146,27 +146,142 @@ function obsanimate9() {
   }, 3000);
 }
 
-// obsanimate9();
+// f9();
 
-// setInterval(obsanimate9, 8000);
+// let clear9 = setInterval(f9, 8000);
 
 //CAR CRASH LOGIC
 
-setInterval(() => {
-  let cy = parseInt(car.getBoundingClientRect().top);
-  let o1y = Math.abs(parseInt(obs1.getBoundingClientRect().top));
+//obs1
+// setInterval(() => {
+//   let c1y = parseInt(car.getBoundingClientRect().top);
+//   let o1y = Math.abs(parseInt(obs1.getBoundingClientRect().top));
 
-  let offsetY = Math.abs(o1y - cy);
+//   let offsetY1 = Math.abs(o1y - c1y);
 
-  console.log(cy);
-  console.log(o1y);
-  console.log(offsetY);
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY1);
   
-  if(offsetY<10) {
-  f1=null;
-  clearInterval(clear1);
-  obs1.style.top=cy + "px";
-  }
+//   if(offsetY1<10) {
+//   f1=null;
+//   clearInterval(clear1);
+//   obs1.style.top=c1y + "px";
+//   }
 
-}, 50);
+// }, 50);
+
+//obs 2
+// setInterval(() => {
+//   let c2y = parseInt(car.getBoundingClientRect().top);
+//   let o2y = Math.abs(parseInt(obs2.getBoundingClientRect().top));
+
+//   let offsetY2 = Math.abs(o2y - c2y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY2<10) {
+//   f2=null;
+//   clearInterval(clear2);
+//   obs2.style.top=c2y + "px";
+//   }
+
+// }, 50);
+
+//obs3
+// setInterval(() => {
+//   let c3y = parseInt(car.getBoundingClientRect().top);
+//   let o3y = Math.abs(parseInt(obs3.getBoundingClientRect().top));
+
+//   let offsetY3 = Math.abs(o3y - c3y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY3<10) {
+//   f3=null;
+//   clearInterval(clear3);
+//   obs3.style.top=c3y + "px";
+//   }
+
+// }, 50);
+
+// //obs4
+// setInterval(() => {
+//   let c4y = parseInt(car.getBoundingClientRect().top);
+//   let o4y = Math.abs(parseInt(obs4.getBoundingClientRect().top));
+
+//   let offsetY4 = Math.abs(o4y - c4y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY4<10) {
+//   f4=null;
+//   clearInterval(clear4);
+//   obs4.style.top=c4y + "px";
+//   }
+
+// }, 50);
+
+// //obs5
+// setInterval(() => {
+//   let c5y = parseInt(car.getBoundingClientRect().top);
+//   let o5y = Math.abs(parseInt(obs5.getBoundingClientRect().top));
+
+//   let offsetY5 = Math.abs(o5y - c5y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY5<10) {
+//   f5=null;
+//   clearInterval(clear5);
+//   obs5.style.top=c5y + "px";
+//   }
+
+// }, 50);
+
+// //obs7
+// setInterval(() => {
+//   let c7y = parseInt(car.getBoundingClientRect().top);
+//   let o7y = Math.abs(parseInt(obs7.getBoundingClientRect().top));
+
+//   let offsetY7 = Math.abs(o7y - c7y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY7<10) {
+//   f7=null;
+//   clearInterval(clear7);
+//   obs7.style.top=c7y + "px";
+//   }
+
+// }, 50);
+
+// //obs9
+// setInterval(() => {
+//   let c9y = parseInt(car.getBoundingClientRect().top);
+//   let o9y = Math.abs(parseInt(obs9.getBoundingClientRect().top));
+
+//   let offsetY9 = Math.abs(o9y - c9y);
+
+//   // console.log(cy);
+//   // console.log(o1y);
+//   // console.log(offsetY);
+  
+//   if(offsetY9<10) {
+//   f9=null;
+//   clearInterval(clear9);
+//   obs9.style.top=c9y + "px";
+//   }
+
+// }, 50);
 
